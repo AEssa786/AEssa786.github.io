46a37fdef9df586a0903424fbea70b907622c167
Abdullah Essa <aessa78644@gmail.com>
2026-06-20 17:47:48 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -21,12 +21,12 @@ export default defineConfig({
             portfolio_contact: "Portfolio/contact.html",
             portfolio_form_complete: "Portfolio/form-complete.html",
 
-          goblinPouch: resolve(__dirname, 'Portfolio/projects/GoblinPouch/details.html'),
-          monthlyClaim: resolve(__dirname, 'Portfolio/projects/MonthlyClaim/details.html'),
-          ecomStore: resolve(__dirname, 'Portfolio/projects/EcomStore/details.html'),
-          eventBooking: resolve(__dirname, 'Portfolio/projects/EventBooking/details.html'),
-          productChecker: resolve(__dirname, 'Portfolio/projects/ProductChecker/details.html'),
-          secureFileShare: resolve(__dirname, 'Portfolio/projects/SecureFileShare/details.html'),
+          goblinPouch: 'Portfolio/projects/GoblinPouch/details.html',
+          monthlyClaim: 'Portfolio/projects/MonthlyClaim/details.html',
+          ecomStore: 'Portfolio/projects/EcomStore/details.html',
+          eventBooking: 'Portfolio/projects/EventBooking/details.html',
+          productChecker: 'Portfolio/projects/ProductChecker/details.html',
+          secureFileShare: 'Portfolio/projects/SecureFileShare/details.html',
 
         }
     }
